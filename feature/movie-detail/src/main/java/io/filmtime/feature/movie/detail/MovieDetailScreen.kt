c57adcb6d27822770da76bb2c6683fc51f772830
Reza Moallemi <rm.allmails@gmail.com>
2023-08-27 21:07:54 -0700

    Enhance Movie Detail UI

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -3,17 +3,18 @@ package io.filmtime.feature.movie.detail
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -30,38 +31,45 @@ fun MovieDetailScreen(
   } else if (videoDetail != null) {
     Column(
       modifier = Modifier
-        .verticalScroll(rememberScrollState())
-        .padding(16.dp),
+        .verticalScroll(rememberScrollState()),
       verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
       AsyncImage(
         modifier = Modifier
-          .width(200.dp)
-          .height(300.dp),
-        model = videoDetail.posterUrl,
-        contentDescription = null,
-      )
-
-      AsyncImage(
-        modifier = Modifier
           .fillMaxWidth()
-          .height(300.dp),
+          .aspectRatio(16f / 9f),
+        contentScale = ContentScale.Crop,
         model = videoDetail.coverUrl,
         contentDescription = null,
       )
 
-      Text(text = videoDetail.title)
-      Text(text = videoDetail.description)
+      Text(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        style = MaterialTheme.typography.titleLarge,
+        text = videoDetail.title,
+      )
+      Text(
+        modifier = Modifier.padding(horizontal = 16.dp),
+
+        text = videoDetail.description,
+      )
 
       Row(
+        modifier = Modifier.padding(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
       ) {
         Text(text = "Year: ${videoDetail.year}")
         Text(text = "Original language: ${videoDetail.originalLanguage}")
         Text(text = videoDetail.spokenLanguages.joinToString(", "))
       }
-      Text(text = "Genres")
-      Text(text = videoDetail.genres.joinToString(", "))
+      Text(
+        modifier = Modifier.padding(start = 16.dp),
+        text = "Genres",
+      )
+      Text(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        text = videoDetail.genres.joinToString(", "),
+      )
     }
   }
 }

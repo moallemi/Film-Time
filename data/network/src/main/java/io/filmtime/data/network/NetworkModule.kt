8dfc140ba99a643c78cc0085876249110af8c79a
Reza Moallemi <rm.allmails@gmail.com>
2023-09-10 20:56:17 -0700

    Add support for NetworkCallAdapterFactory for TMDB network requests

--- data/network/src/main/java/io/filmtime/data/network/NetworkModule.kt
+++ data/network/src/main/java/io/filmtime/data/network/NetworkModule.kt
@@ -5,8 +5,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.filmtime.data.network.adapter.NetworkCallAdapterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -26,10 +28,12 @@ object NetworkModule {
   @Singleton
   fun providesRetrofit(
     json: Json,
+    networkCallAdapterFactory: CallAdapter.Factory,
   ): Retrofit {
     return Retrofit.Builder()
       .baseUrl("https://api.themoviedb.org/3/")
       .addConverterFactory(json.asConverterFactory(MediaType.get("application/json")))
+      .addCallAdapterFactory(networkCallAdapterFactory)
       .build()
   }
 
@@ -37,5 +41,11 @@ object NetworkModule {
   @Singleton
   fun providesTmdbMovieService(retrofit: Retrofit): TmdbMoviesService {
     return retrofit.create(TmdbMoviesService::class.java)
+  }
+
+  @Provides
+  @Singleton
+  fun providesNetworkCallAdapterFactory(): CallAdapter.Factory {
+    return NetworkCallAdapterFactory()
   }
 }

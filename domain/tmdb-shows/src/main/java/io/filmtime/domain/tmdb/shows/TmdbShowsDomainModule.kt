b24736015e325d7299a4e36e63e827b7473abb29
Reza Moallemi <rm.allmails@gmail.com>
2023-10-01 09:37:34 -0700

    #1 Add tmdb domain module

---
+++ domain/tmdb-shows/src/main/java/io/filmtime/domain/tmdb/shows/TmdbShowsDomainModule.kt
@@ -0,0 +1,14 @@
+package io.filmtime.domain.tmdb.shows
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class TmdbShowsDomainModule {
+
+  @Binds
+  abstract fun bindGetTrendingShowsUseCase(impl: GetTrendingShowsUseCaseImpl): GetTrendingShowsUseCase
+}

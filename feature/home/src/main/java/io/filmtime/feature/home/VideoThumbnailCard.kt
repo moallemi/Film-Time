9d46c2a8b33ea0479812c8d0528446e5475b9ccc
Hadi Norouzi <h.noroozi@ernyka.com>
2023-10-02 18:58:07 +0330

    video card scale animation on focus

--- feature/home/src/main/java/io/filmtime/feature/home/VideoThumbnailCard.kt
+++ feature/home/src/main/java/io/filmtime/feature/home/VideoThumbnailCard.kt
@@ -1,10 +1,18 @@
 package io.filmtime.feature.home
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImage
 import io.filmtime.data.model.VideoThumbnail
@@ -16,9 +24,17 @@ fun VideoThumbnailCard(
   videoThumbnail: VideoThumbnail,
   onClick: () -> Unit,
 ) {
+  var selected by remember { mutableStateOf(false) }
+  val scale by animateFloatAsState(if (selected) 1.1f else 1f, label = "scale-transition")
+
   Card(
     onClick = onClick,
-    modifier = modifier,
+    modifier = modifier
+      .scale(scale)
+      .onFocusChanged {
+        selected = it.isFocused
+      }
+      .focusable(),
   ) {
     VideoThumbnailCardContent(videoThumbnail = videoThumbnail)
   }

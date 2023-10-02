859985dc1175a523f9dd72488f08b227d416955a
Hadi Norouzi <hadinorouzi78@gmail.com>
2023-10-03 00:15:06 +0330

    add poster image in movie detail page

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -73,16 +76,32 @@ fun MovieDetailContent(
     modifier = Modifier.verticalScroll(rememberScrollState()),
     verticalArrangement = Arrangement.spacedBy(16.dp),
   ) {
-    Box {
-
+    Box(
+      modifier = Modifier
+        .height(300.dp)
+    ) {
       AsyncImage(
         modifier = Modifier
           .fillMaxWidth()
           .aspectRatio(16f / 9f),
-        contentScale = ContentScale.Crop,
+        contentScale = ContentScale.FillWidth,
         model = videoDetail.coverUrl,
         contentDescription = null,
+        alignment = Alignment.BottomCenter,
       )
+      Card(
+        modifier = Modifier
+          .align(Alignment.BottomStart)
+          .padding(start = 16.dp)
+      ) {
+        AsyncImage(
+          modifier = Modifier
+            .size(120.dp, 200.dp),
+          contentScale = ContentScale.Crop,
+          model = videoDetail.posterUrl,
+          contentDescription = null,
+        )
+      }
       IconButton(onClick = onBackPressed) {
         Icon(Icons.Filled.ArrowBack, contentDescription = "back")
       }

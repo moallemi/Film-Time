1d1d8da575677f5c00ce26e1de0580d8ee464b62
Hadi Norouzi <h.noroozi@ernyka.com>
2023-10-04 11:48:09 +0330

    Update java version, add Trakt client id from env

--- feature/trakt-login/build.gradle.kts
+++ feature/trakt-login/build.gradle.kts
@@ -6,13 +6,24 @@ plugins {
 
 android {
   namespace = "io.filmtime.feature.trakt.login"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     minSdk = 27
 
+    buildConfigField("String", "TRAKT_CLIENT_ID", "\"${System.getenv("FILM_TIME_TRAKT_CLIENT_ID")}\"")
+
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildFeatures {
+    compose = true
+    buildConfig = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.3"
   }
 
   buildTypes {
@@ -25,11 +36,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
 }
 

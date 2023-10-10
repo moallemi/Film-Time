6911af79f56aa55ff9b4151ca91f05888d33fc63
m.rajaeian <m.rajaeian@ernyka.com>
2023-10-10 12:57:56 +0330

    Handle Errors in MovieDetailScreen

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,6 +22,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,9 +31,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import io.filmtime.data.model.GeneralError
 import io.filmtime.data.model.VideoDetail
 
 @Composable
@@ -53,8 +61,10 @@ fun MovieDetailScreen(
     CircularProgressIndicator(
       modifier = Modifier.wrapContentSize(),
     )
-  } else if (state.message != null) {
-    Text(text = state.message!!)
+  } else if (state.error != null) {
+    ShowError(state.error!!, state.message!!) {
+      viewModel.load()
+    }
   } else if (videoDetail != null) {
     MovieDetailContent(
       videoDetail = videoDetail,
@@ -62,6 +72,45 @@ fun MovieDetailScreen(
       onBackPressed = onBackPressed,
       onPlayPressed = viewModel::loadStreamInfo,
     )
+  }
+}
+
+@Composable
+fun ShowError(error: GeneralError, message: String, onRefresh: () -> Unit) {
+  val composition by rememberLottieComposition(
+    LottieCompositionSpec.RawRes(
+      if (error is GeneralError.NetworkError) {
+        R.raw.network_lost
+      } else {
+        R.raw.not_found
+      },
+    ),
+  )
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(horizontal = 13.dp),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+
+  ) {
+    LottieAnimation(
+      modifier = Modifier.size(size = 240.dp),
+      composition = composition,
+    )
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(
+      text = message,
+      textAlign = TextAlign.Center,
+      style = typography.bodyLarge,
+    )
+
+    Spacer(modifier = Modifier.size(60.dp))
+    Button(
+      onClick = onRefresh,
+    ) {
+      Text(text = "Refresh")
+    }
   }
 }
 
@@ -109,7 +158,7 @@ fun MovieDetailContent(
 
     Text(
       modifier = Modifier.padding(horizontal = 16.dp),
-      style = MaterialTheme.typography.titleLarge,
+      style = typography.titleLarge,
       text = videoDetail.title,
     )
     Text(

f2cdcc938ea8541fa4edb0c8929a6b4dfc3df388
m.rajaeian <m.rajaeian@ernyka.com>
2023-10-14 12:51:28 +0330

    implementErrorScreenInShows

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

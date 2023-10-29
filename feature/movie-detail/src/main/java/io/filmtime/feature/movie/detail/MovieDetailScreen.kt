242491823c23ee96f0248d5609e07df824a2c798
m.rajaeian <m.rajaeian@ernyka.com>
2023-10-29 18:04:43 +0330

    fixReviewIssues

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -21,17 +21,21 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.airbnb.lottie.compose.LottieAnimation
@@ -61,9 +65,7 @@ fun MovieDetailScreen(
       modifier = Modifier.wrapContentSize(),
     )
   } else if (state.error != null) {
-    ShowError(state.error!!, state.message!!) {
-      viewModel.load()
-    }
+    ShowError(error = state.error!!, message = state.message!!, onRefresh = viewModel::load)
   } else if (videoDetail != null) {
     MovieDetailContent(
       videoDetail = videoDetail,
@@ -88,27 +90,30 @@ fun ShowError(error: GeneralError, message: String, onRefresh: () -> Unit) {
   Column(
     modifier = Modifier
       .fillMaxSize()
-      .padding(horizontal = 13.dp),
+      .padding(horizontal = 16.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
 
   ) {
     LottieAnimation(
-      modifier = Modifier.size(size = 240.dp),
+      modifier = Modifier.scale(0.8f),
       composition = composition,
     )
-    Spacer(modifier = Modifier.size(10.dp))
+    Spacer(modifier = Modifier.size(8.dp))
     Text(
       text = message,
       textAlign = TextAlign.Center,
-      style = typography.bodyLarge,
+      style = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+      ),
     )
 
     Spacer(modifier = Modifier.size(60.dp))
     Button(
       onClick = onRefresh,
     ) {
-      Text(text = "Refresh")
+      Text(text = stringResource(R.string.refresh))
     }
   }
 }
@@ -157,7 +162,10 @@ fun MovieDetailContent(
 
     Text(
       modifier = Modifier.padding(horizontal = 16.dp),
-      style = typography.titleLarge,
+      style = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+      ),
       text = videoDetail.title,
     )
     Text(

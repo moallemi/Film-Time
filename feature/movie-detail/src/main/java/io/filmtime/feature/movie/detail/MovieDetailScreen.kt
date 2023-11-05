57ae990b010a1a18d467ed6260b6a39c0bba6eff
hadi-norouzi <hadinorouzi78@gmail.com>
2023-11-05 15:09:27 +0330

    fix lints

--- feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
+++ feature/movie-detail/src/main/java/io/filmtime/feature/movie/detail/MovieDetailScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

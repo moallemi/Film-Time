891c4a60f61998a56b19481f0e751aedd8fa59f6
Sobhan Mohammadpour <sobhanmohamadpour@gmail.com>
2023-11-23 08:21:06 +0100

    fix: adding trailing commas back

--- data/network/src/main/java/io/filmtime/data/network/TmdbShowsService.kt
+++ data/network/src/main/java/io/filmtime/data/network/TmdbShowsService.kt
@@ -3,17 +3,16 @@ package io.filmtime.data.network
 import io.filmtime.data.network.adapter.NetworkResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface TmdbShowsService {
 
   @GET("tv/{series_id})")
   suspend fun getShowDetails(
-    @Path("series_id") seriesId: Int
+    @Path("series_id") seriesId: Int,
   ): NetworkResponse<TmdbShowDetailsResponse, TmdbErrorResponse>
 
   @GET("trending/tv/{time_window}")
   suspend fun getTrendingShows(
-    @Path("time_window") timeWindow: String
+    @Path("time_window") timeWindow: String,
   ): NetworkResponse<TmdbShowListResponse, TmdbErrorResponse>
 }

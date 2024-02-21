48137b7253585714e9a5685aacdac204e6132fe2
Reza Moallemi <rm.allmails@gmail.com>
2024-02-20 20:21:44 -0800

    #46 Add movies feature module

---
+++ feature/movies/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+  alias(libs.plugins.com.android.library)
+  alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+  namespace = "io.filmtime.feature.movies"
+  compileSdk = 34
+
+  defaultConfig {
+    minSdk = 27
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+}
+
+dependencies {
+
+  implementation(libs.core.ktx)
+  implementation(libs.appcompat)
+  implementation(libs.material)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}

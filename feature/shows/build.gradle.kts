28f57f2f1a367df151f63060d8c01ba921ae61e6
Reza Moallemi <rm.allmails@gmail.com>
2024-02-23 14:02:14 -0800

    #48 Add shows feature module

---
+++ feature/shows/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+  alias(libs.plugins.com.android.library)
+  alias(libs.plugins.org.jetbrains.kotlin.android)
+
+  kotlin("kapt")
+  alias(libs.plugins.hilt.android)
+}
+
+android {
+  namespace = "io.filmtime.feature.shows"
+  compileSdk = 34
+
+  defaultConfig {
+    minSdk = 27
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.9"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+}
+
+dependencies {
+
+  implementation(project(":data:model"))
+  implementation(project(":domain:tmdb-shows"))
+  implementation(project(":core:ui:common"))
+
+  implementation(libs.core.ktx)
+  implementation(libs.appcompat)
+  implementation(libs.material)
+
+  implementation(platform(libs.compose.bom))
+  implementation(libs.ui)
+  implementation(libs.ui.graphics)
+  implementation(libs.ui.tooling.preview)
+  implementation(libs.material3)
+
+  implementation(libs.coil.compose)
+
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.androidx.hilt.navigation.compose)
+
+  implementation(libs.hilt.android)
+  kapt(libs.dagger.hilt.android.compiler)
+
+  implementation(libs.lifecycle.viewmodel.compose)
+  implementation(libs.lifecycle.viewmodel.compose.runtime)
+
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}

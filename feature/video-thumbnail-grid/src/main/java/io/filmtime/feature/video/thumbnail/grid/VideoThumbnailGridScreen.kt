44749b018a1a96ac87474f313b82b34fbefcc3d2
Reza Moallemi <rm.allmails@gmail.com>
2024-04-19 18:21:31 -0700

    #54 Add back button to VideoThumbnailGrid screen toolbar

--- feature/video-thumbnail-grid/src/main/java/io/filmtime/feature/video/thumbnail/grid/VideoThumbnailGridScreen.kt
+++ feature/video-thumbnail-grid/src/main/java/io/filmtime/feature/video/thumbnail/grid/VideoThumbnailGridScreen.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -56,6 +60,16 @@ private fun VideoThumbnailGridScreen(
     topBar = {
       TopAppBar(
         title = { Text(text = state.title) },
+        navigationIcon = {
+          IconButton(
+            onClick = onBack,
+          ) {
+            Icon(
+              imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+              contentDescription = "Back",
+            )
+          }
+        },
       )
     },
     content = { padding ->

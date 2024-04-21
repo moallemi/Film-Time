2b3721781d733473fcc3f7ae3d8b6fe5dab0371c
Reza Moallemi <rm.allmails@gmail.com>
2024-04-21 12:20:04 -0700

    #50 Add trakt card to settings screen

--- feature/settings/src/main/java/io/filmtime/feature/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/io/filmtime/feature/settings/SettingsScreen.kt
@@ -1,24 +1,62 @@
 package io.filmtime.feature.settings
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.filmtime.core.designsystem.composable.FilmTimeSmallTopAppBar
+import io.filmtime.core.designsystem.theme.PreviewFilmTimeTheme
+import io.filmtime.core.designsystem.theme.ThemePreviews
+import io.filmtime.feature.settings.components.TraktCard
 
 @Composable
-fun SettingsScreen() {
-  Box(
-    modifier = Modifier
-      .fillMaxSize(),
+fun SettingsScreen(modifier: Modifier = Modifier) {
+  SettingsScreen()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsScreen() {
+  Scaffold(
+    topBar = {
+      FilmTimeSmallTopAppBar(
+        title = stringResource(R.string.feature_settings_settings),
+      )
+    },
+  ) { padding ->
+    SettingsContent(
+      contentPadding = padding,
+    )
+  }
+}
+
+@Composable
+private fun SettingsContent(
+  contentPadding: PaddingValues,
+) {
+  LazyColumn(
+    contentPadding = contentPadding,
   ) {
-    Text(
-      modifier = Modifier
-        .align(Alignment.Center),
-      text = "Settings",
-      style = MaterialTheme.typography.titleLarge,
+    item {
+      TraktCard(
+        modifier = Modifier
+          .padding(16.dp),
+      )
+    }
+  }
+}
+
+@ThemePreviews
+@Composable
+private fun SettingsScreenPreview() {
+  PreviewFilmTimeTheme {
+    SettingsContent(
+      contentPadding = PaddingValues(16.dp),
     )
   }
 }

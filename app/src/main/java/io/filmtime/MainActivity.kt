e47c999190dd8d658ac231df88c698f5803faee5
Reza Moallemi <rm.allmails@gmail.com>
2024-05-16 16:13:15 -0700

    Add edge to edge support

--- app/src/main/java/io/filmtime/MainActivity.kt
+++ app/src/main/java/io/filmtime/MainActivity.kt
@@ -3,6 +3,7 @@ package io.filmtime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.filmtime.core.designsystem.theme.FilmTimeTheme
@@ -12,6 +13,8 @@ import io.filmtime.ui.FilmTimeApp
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
 
     setContent {
       FilmTimeTheme {

58a74d16d7dfd80521d8efda11524569942358db
targholi <targholi@behsazan.net>
2024-06-15 16:59:45 +0330

    fix Long video thumbnail titles issues.

--- feature/show-detail/src/main/java/io/filmtime/feature/show/detail/ShowDetailScreen.kt
+++ feature/show-detail/src/main/java/io/filmtime/feature/show/detail/ShowDetailScreen.kt
@@ -1,11 +1,13 @@
 package io.filmtime.feature.show.detail
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,6 +35,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntSize
@@ -83,6 +86,7 @@ fun ShowDetailScreen(
   }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ShowDetailContent(
   videoDetail: VideoDetail,
@@ -227,7 +231,10 @@ fun ShowDetailContent(
       ) {
         items(similarState.videoItems) { item ->
           VideoThumbnailCard(
-            modifier = Modifier,
+            modifier = Modifier
+              .animateItemPlacement()
+              .fillParentMaxHeight()
+              .aspectRatio(2 / 3f),
             videoThumbnail = item,
             onClick = {
               item.ids.tmdbId?.let {

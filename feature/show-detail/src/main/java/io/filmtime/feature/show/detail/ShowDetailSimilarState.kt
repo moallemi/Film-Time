8051985c217b18cb0cc9de9bcb3d5f7cde27daf0
Reza Moallemi <rm.allmails@gmail.com>
2024-07-05 21:51:49 -0700

    #74 Remove loading similar and credits from ShowDetailViewModel

--- feature/show-detail/src/main/java/io/filmtime/feature/show/detail/ShowDetailSimilarState.kt
+++
@@ -1,9 +0,0 @@
-package io.filmtime.feature.show.detail
-
-import io.filmtime.data.model.VideoThumbnail
-
-data class ShowDetailSimilarState(
-  val isLoading: Boolean = false,
-  val errorMessage: String = "",
-  val videoItems: List<VideoThumbnail> = emptyList(),
-)
--- feature/show-detail/src/main/java/io/filmtime/feature/show/detail/ShowDetailViewModel.kt
+++ feature/show-detail/src/main/java/io/filmtime/feature/show/detail/ShowDetailViewModel.kt
@@ -5,17 +5,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.filmtime.core.ui.common.toUiMessage
-import io.filmtime.data.model.GeneralError
-import io.filmtime.data.model.Result.Failure
-import io.filmtime.data.model.Result.Success
 import io.filmtime.data.model.VideoType.Show
 import io.filmtime.domain.bookmarks.AddBookmarkUseCase
 import io.filmtime.domain.bookmarks.DeleteBookmarkUseCase
 import io.filmtime.domain.bookmarks.ObserveBookmarkUseCase
 import io.filmtime.domain.tmdb.shows.GetEpisodesBySeasonUseCase
-import io.filmtime.domain.tmdb.shows.GetShowCreditsUseCase
 import io.filmtime.domain.tmdb.shows.GetShowDetailsUseCase
-import io.filmtime.domain.tmdb.shows.GetSimilarShowsUseCase
 import io.filmtime.domain.trakt.GetRatingsUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -29,8 +24,6 @@ import javax.inject.Inject
 internal class ShowDetailViewModel @Inject constructor(
   savedStateHandle: SavedStateHandle,
   private val getShowDetails: GetShowDetailsUseCase,
-  private val getShowCreditsUseCase: GetShowCreditsUseCase,
-  private val getSimilarShowsUseCase: GetSimilarShowsUseCase,
   private val addBookmark: AddBookmarkUseCase,
   private val deleteBookmark: DeleteBookmarkUseCase,
   private val observeBookmark: ObserveBookmarkUseCase,
@@ -41,100 +34,10 @@ internal class ShowDetailViewModel @Inject constructor(
   private val videoId: Int = savedStateHandle["video_id"] ?: throw IllegalStateException("videoId is required")
   private val _state: MutableStateFlow<ShowDetailState> = MutableStateFlow(ShowDetailState())
   val state = _state.asStateFlow()
-  private val _similarState: MutableStateFlow<ShowDetailSimilarState> = MutableStateFlow(ShowDetailSimilarState())
-  val similarState = _similarState.asStateFlow()
-
-  private val _creditState: MutableStateFlow<ShowDetailCreditState> = MutableStateFlow(ShowDetailCreditState())
-  val creditState = _creditState.asStateFlow()
 
   init {
     observeBookmark()
     load()
-    loadSimilar()
-    loadCredits()
-  }
-
-  private fun loadSimilar() = viewModelScope.launch {
-    _similarState.value = _similarState.value.copy(isLoading = true)
-    when (val result = getSimilarShowsUseCase(videoId)) {
-      is Failure -> {
-        when (result.error) {
-          is GeneralError.ApiError -> {
-            _similarState.update { state ->
-              state.copy(
-                errorMessage = (result.error as GeneralError.ApiError).message.orEmpty(),
-                isLoading = false,
-              )
-            }
-          }
-
-          GeneralError.NetworkError -> {
-            _similarState.update { state ->
-              state.copy(
-                errorMessage = "No internet connection. Please check your network settings.",
-                isLoading = false,
-              )
-            }
-          }
-
-          is GeneralError.UnknownError -> _similarState.update { state ->
-            state.copy(
-              errorMessage = (result.error as GeneralError.UnknownError).error.message.orEmpty(),
-              isLoading = false,
-            )
-          }
-        }
-      }
-
-      is Success -> {
-        _similarState.update { state ->
-          state.copy(videoItems = result.data, isLoading = false, errorMessage = "")
-        }
-      }
-    }
-  }
-
-  private fun loadCredits() = viewModelScope.launch {
-    _creditState.value = _creditState.value.copy(isLoading = true)
-    when (val result = getShowCreditsUseCase(videoId)) {
-      is Failure -> {
-        when (result.error) {
-          is GeneralError.ApiError -> {
-            _creditState.update { state ->
-              state.copy(
-                error = result.error,
-                message = (result.error as GeneralError.ApiError).message,
-                isLoading = false,
-              )
-            }
-          }
-
-          GeneralError.NetworkError -> {
-            _creditState.update { state ->
-              state.copy(
-                error = result.error,
-                message = "No internet connection. Please check your network settings.",
-                isLoading = false,
-              )
-            }
-          }
-
-          is GeneralError.UnknownError -> _creditState.update { state ->
-            state.copy(
-              error = result.error,
-              message = (result.error as GeneralError.UnknownError).error.message,
-              isLoading = false,
-            )
-          }
-        }
-      }
-
-      is Success -> {
-        _creditState.update { state ->
-          state.copy(credit = result.data, isLoading = false)
-        }
-      }
-    }
   }
 
   fun load() = viewModelScope.launch {

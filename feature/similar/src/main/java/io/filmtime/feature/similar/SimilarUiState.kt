b62fd09ba313647de858fd6f0c0d0c8453132908
Reza Moallemi <rm.allmails@gmail.com>
2026-07-07 08:29:00 -0700

    #149 Migrate feature/similar to MVI

--- feature/similar/src/main/java/io/filmtime/feature/similar/SimilarUiState.kt
+++ feature/similar/src/main/java/io/filmtime/feature/similar/SimilarUiState.kt
@@ -3,7 +3,7 @@ package io.filmtime.feature.similar
 import io.filmtime.core.ui.common.UiMessage
 import io.filmtime.data.model.VideoThumbnail
 
-data class SimilarUiState(
+internal data class SimilarUiState(
   val isLoading: Boolean = false,
   val videoItems: List<VideoThumbnail> = emptyList(),
   val error: UiMessage? = null,
--- feature/similar/src/main/java/io/filmtime/feature/similar/SimilarViewModel.kt
+++ feature/similar/src/main/java/io/filmtime/feature/similar/SimilarViewModel.kt
@@ -1,18 +1,19 @@
 package io.filmtime.feature.similar
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.filmtime.core.ui.common.extensions.launch
 import io.filmtime.core.ui.common.toUiMessage
 import io.filmtime.data.model.Result.Failure
 import io.filmtime.data.model.Result.Success
 import io.filmtime.data.model.VideoType
 import io.filmtime.domain.tmdb.movies.GetSimilarMoviesUseCase
 import io.filmtime.domain.tmdb.shows.GetSimilarShowsUseCase
+import io.filmtime.feature.similar.SimilarAction.LoadSimilar
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,13 +22,29 @@ internal class SimilarViewModel @Inject constructor(
   private val getSimilarShows: GetSimilarShowsUseCase,
 ) : ViewModel() {
 
+  private val pendingActions = MutableSharedFlow<SimilarAction>()
+
   private val _similarState = MutableStateFlow(SimilarUiState())
   val state = _similarState.asStateFlow()
 
-  fun loadSimilar(
+  init {
+    collectActions()
+  }
+
+  fun submitAction(action: SimilarAction) = launch { pendingActions.emit(action) }
+
+  private fun collectActions() = launch {
+    pendingActions.collect { action ->
+      when (action) {
+        is LoadSimilar -> loadSimilar(action.videoId, action.videoType)
+      }
+    }
+  }
+
+  private fun loadSimilar(
     videoId: Int,
     videoType: VideoType,
-  ) = viewModelScope.launch {
+  ) = launch {
     _similarState.value = _similarState.value.copy(isLoading = true, error = null)
 
     val result = if (videoType == VideoType.Movie) {
